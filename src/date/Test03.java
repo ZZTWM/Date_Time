1c5fadfcf67bf8e31ae5f269563e75a6d7dd546b
TWM <15152806231@163.com>
2019-08-28 16:36:01 +0800

    获取当前日期毫秒数

---
+++ src/date/Test03.java
@@ -0,0 +1,17 @@
+package date;
+
+import java.util.Date;
+/**
+ * 当前日期毫秒数
+ * @author Administrator
+ *
+ */
+public class Test03 {
+	public static void main(String[] args) {
+		Date date = new Date();
+		//当前日期的毫秒数
+		System.out.println("Date.getTime() \t\t\t返回值: " + date.getTime());
+		//通过System.currentTimeMillis()获取当前日期的毫秒数
+		System.out.println("System.currentTimeMillis() \t返回值: " + System.currentTimeMillis());
+	}
+}
